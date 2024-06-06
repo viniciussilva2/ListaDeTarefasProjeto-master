1348e90714fcecb4363ccb1bbbfe9bc933af95ba
Vinicius Silva <vinyoli.vs@gmail.com>
2024-06-05 22:22:33 -0300

    #Ajuste de botão LIMPAR
    Alterando botão limpar, adicionando uma função JavaScript para limpar os filtros, o botão criado estava apenas retornando para o Index, fazendo carregamento da página, um comportamento desnecessário.

--- ListaDeTarefasProjetoVideo/wwwroot/js/site.js
+++ ListaDeTarefasProjetoVideo/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.getElementById('limparFiltros').addEventListener('click', function () {
+    // Reseta os valores dos campos de filtro
+    document.getElementsByName('filtro').forEach(function (element) {
+        element.value = 'todos';
+    });
+});
